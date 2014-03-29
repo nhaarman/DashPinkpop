6307d1057b1de84491fea3fa3c0ef92ff040661a
Niek Haarman <haarman.niek@gmail.com>
2014-03-29 18:17:06 +0100

    Update the extension every minute

--- app/src/main/java/com/nhaarman/dashclock/pinkpop/PinkPopExtension.java
+++ app/src/main/java/com/nhaarman/dashclock/pinkpop/PinkPopExtension.java
@@ -1,6 +1,9 @@
 package com.nhaarman.dashclock.pinkpop;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 
 import com.google.android.apps.dashclock.api.DashClockExtension;
@@ -10,11 +13,16 @@ import com.nhaarman.dashclock.pinkpop.preferences.Preferences;
 
 public class PinkPopExtension extends DashClockExtension {
 
+    private static final String CONTENT = "content://";
+
     private static final String WEB_URL = "http://www.pinkpop.nl";
+    private MyBroadcastReceiver mReceiver;
 
     @Override
     protected void onInitialize(final boolean isReconnect) {
         setUpdateWhenScreenOn(true);
+        addWatchContentUris(new String[]{CONTENT + getString(R.string.authority)});
+        mReceiver = new MyBroadcastReceiver();
     }
 
     @Override
@@ -30,10 +38,21 @@ public class PinkPopExtension extends DashClockExtension {
         data.clickIntent(createClickIntent());
 
         publishUpdate(data);
-    }
 
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_TIME_TICK);
+        registerReceiver(mReceiver, intentFilter);
+    }
 
     private static Intent createClickIntent() {
         return new Intent(Intent.ACTION_VIEW, Uri.parse(WEB_URL));
+    }
+
+    private static class MyBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            context.unregisterReceiver(this);
+            context.getContentResolver().notifyChange(Uri.parse(CONTENT + context.getString(R.string.authority)), null);
+        }
     }
 }
